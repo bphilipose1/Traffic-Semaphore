04a9c279a33df62ef534c5fc614c9e8820a3e826
yalemu <93763372+yalemu@users.noreply.github.com>
2023-02-20 17:47:49 -0800

    Create converter.cpp

---
+++ converter.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<time.h>
+#include<string>
+
+
+
+string converter(time_t convert){
+    time_t convert = time(nullptr); // get the current time as a time_t value
+    struct tm* timeinfo = localtime(&convert); // convert to local time
+    int hours = timeinfo->tm_hour; // extract hours
+    int minutes = timeinfo->tm_min; // extract minutes
+    int seconds = timeinfo->tm_sec; // extract seconds
+    string str1 = to_string(hours);
+    string str2 = to_string(minutes);
+    string str3 = to_string(seconds);
+    
+
+    string time = str1 + ':' + str2 + ':' + str3;
+    
+
+    return time; 
+}
