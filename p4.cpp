8d645ed49404523d3709f914fc583e9130944051
Benjamin Philipose <philiposebenjamin@gmail.com>
2023-02-22 11:47:57 -0800

    issue is carthread not being made for1 car

--- p4.cpp
+++ p4.cpp
@@ -38,17 +38,12 @@ queue<car*> northTrafficQueue;
 queue<car*> southTrafficQueue;
 int totalProduced = 0;
 
-int randomizer = 0;//dont care about race condition with this, we want randomization. What better than to use race condition randomness :)
-
-
-
 /*
-NOTES: PROGRAM WILL ONLY END WHEN BOTH PRODUCERS LAND ON TRUE FOR THE 80% COIN FLIP BECAUSE THEN IT WILL CHECK IF IT CAN MAKE ANOTHER CAR, WHICH WILL BE A NO, AND THEN THE PRODUCER FUNCTION WILL EXIT*/
-
+NOTES: PROGRAM WILL ONLY END WHEN BOTH PRODUCERS LAND ON TRUE FOR THE 80% COIN FLIP BECAUSE THEN IT WILL CHECK IF IT CAN MAKE ANOTHER CAR, WHICH WILL BE A NO, AND THEN THE PRODUCER FUNCTION WILL EXIT
+*/
 
 // mutex locks
 pthread_mutex_t mutexCarLog;
-pthread_mutex_t mutexCarProduce;
 pthread_mutex_t mutexQueue; //for both queues 
 
 // counting semaphore
@@ -63,9 +58,7 @@ string converter(time_t convert){ // get the current time as a time_t value
     string str1 = to_string(hours);
     string str2 = to_string(minutes);
     string str3 = to_string(seconds);
-    
     string time = str1 + ':' + str2 + ':' + str3;
-    
     return time; 
 }
 int pthread_sleep (int seconds) {
@@ -85,8 +78,6 @@ int pthread_sleep (int seconds) {
     return pthread_cond_timedwait(&conditionvar, &mutex, &timetoexpire);
 }
 bool eightyCoin()   {   
-
-    srand(time(nullptr) + (++randomizer));
     int coin = rand() % 100;
     if(coin < 80)   {
         return true;
@@ -98,7 +89,6 @@ bool eightyCoin()   {
 void Logcar(int ID,char Dir, string arrival_time, string start_time, string end_time){
     pthread_mutex_lock(&mutexCarLog);
 
-
     ofstream outdata("car.log",ios::app);
     outdata.is_open();
     if (!outdata)
@@ -107,11 +97,12 @@ void Logcar(int ID,char Dir, string arrival_time, string start_time, string end_
         exit(1);
     }
     outdata << ID << setw(7) << Dir << setw(10) <<arrival_time << setw(10) <<start_time << setw(10) << end_time << '\n';
+    outdata.flush();
     outdata.close();
+
     pthread_mutex_unlock(&mutexCarLog);
 }
 void Logflagperson(string timestamp, string status){
-
     ofstream outdata("flagperson.log",ios::app);
     outdata.is_open();
     if (!outdata)
@@ -120,20 +111,21 @@ void Logflagperson(string timestamp, string status){
         exit(1);
     }
     outdata << timestamp << setw(15) << status << endl;
+    outdata.flush();
     outdata.close();
 }
-void* carCross(void*arg) {   
-    
+void* carCross(void*arg) {     
+
     car* my_car = (car*)(arg);
-    pthread_detach(pthread_self());
 
     time_t s_time = time(0);//time car starts to cross construction lane   
-
+    
     pthread_sleep(2);//car is crossing construction lane
     time_t e_time = time(0);//time car finishes crossing construction lane
+    
     Logcar(my_car->carID, my_car->directions, converter(my_car->arrivalTime), converter(s_time), converter(e_time));
 
-    
+
     delete my_car;  //deallocate car object after completing crossing
     return NULL;
 }
@@ -145,22 +137,20 @@ void* northCarGenerator(void* totaC) {
         if (eightyCoin() == true) {
             
             //check if producer should keep making cars
-            pthread_mutex_lock(&mutexCarProduce);
+            pthread_mutex_lock(&mutexQueue);
             if((totalProduced+1) > totalCars) {
                 GoN = false;
-                pthread_mutex_unlock(&mutexCarProduce);
+                pthread_mutex_unlock(&mutexQueue);
                 pthread_exit(NULL);
             }
             else    {
                 totalProduced++;
             }
-            pthread_mutex_unlock(&mutexCarProduce);
-            
-            pthread_mutex_lock(&mutexQueue);    //grab lock to modify queues
 
             car* newCar = new car(totalProduced, 'N', time(nullptr));   //create car object DYNAMICALLY
                         
             if(newCar != nullptr) {
+
                 northTrafficQueue.push(newCar); //add car into queue
                 
             } else {
@@ -172,6 +162,7 @@ void* northCarGenerator(void* totaC) {
             
         }   
         else {
+
             pthread_sleep(21); // sleep if another car does not follow
         }
     }
@@ -180,71 +171,54 @@ void* northCarGenerator(void* totaC) {
 void* southCarGenerator(void* totaC) {
     int totalCars = *((int*)totaC);//cast input parameters
     bool GoS = true;
-    
     while (GoS) {   
         if (eightyCoin() == true) {
-
             //check if producer should keep making cars
-            pthread_mutex_lock(&mutexCarProduce);
+            pthread_mutex_lock(&mutexQueue);
             if((totalProduced+1) > totalCars) {
                 GoS = false;
-                pthread_mutex_unlock(&mutexCarProduce);
+                pthread_mutex_unlock(&mutexQueue);
                 pthread_exit(NULL);
             }
             else    {
                 totalProduced++;
             }
-            pthread_mutex_unlock(&mutexCarProduce);
-
-            pthread_mutex_lock(&mutexQueue);    //grab lock to modify queues
             car* newCar = new car(totalProduced, 'S', time(nullptr));   //create car object DYNAMICALLY
-
             if(newCar != nullptr) {
-                southTrafficQueue.push(newCar); //add car into queue
 
+                southTrafficQueue.push(newCar); //add car into queue
             } else {
                 cout << "Failed to create a new car object." << endl;
             }
             pthread_mutex_unlock(&mutexQueue);  //return lock after modifying queues
-            
-            
-            
-
             sem_post(&isEmpty);
         }
         else {
-
+            
             pthread_sleep(21); // sleep if another car does not follow
-
         }
     }
     pthread_exit(NULL);
 }
 void* flagHandler(void* x) {
-
     int carCnt = 0;
     char laneState = 'N';   //used to state which lane is currently being allowed to pass
     int n_size = 0;
     int s_size = 0;
-    car* carTemp;
-    
-    
-    int totCars = *((int*) x); //see how many cars are allowed to pass cumulative
-    //creates threads for cumulative total of cars
-    vector<pthread_t> carThreads(totCars);
-
     clock_t tempSleepTime = 0;
     clock_t tempAwakeTime = 0;
-
-
+    car* carTemp;
+    
+    int totCars = *((int*) x); //see how many cars are allowed to pass cumulative
+    
+    //creates threads for cumulative total of cars
+    vector<pthread_t> carThreads(totCars);
     
     while(totCars != carCnt)    {
-        carCnt++;
-        
+        carCnt++;    
+
         tempSleepTime = time(nullptr);
-
         sem_wait(&isEmpty);   //will sleep thread if there is no cars waiting in either queues
-
         tempAwakeTime = time(nullptr);
         
         pthread_mutex_lock(&mutexQueue);
@@ -281,31 +255,25 @@ void* flagHandler(void* x) {
         pthread_mutex_unlock(&mutexQueue);
 
         //creates car thread for car object
-
-        
         if(pthread_create(&carThreads[(carCnt-1)], NULL, &carCross, (void *)carTemp)) {
             perror("Pthread_create failed");
             exit(-1);
         }
-        pthread_sleep(2);   //waits 1 second before sending next car
-        
+
+        pthread_detach(carThreads[(carCnt-1)]);        
         if(tempSleepTime < tempAwakeTime)  {// logging flagperson behavior
             Logflagperson(converter(tempSleepTime), "sleep");
             Logflagperson(converter(tempAwakeTime), "woken-up");
         }
-        
     }
-    
     return NULL;
 }
 int getNorthSize()   {
-    int nSize = northTrafficQueue.size();
-             
+    int nSize = northTrafficQueue.size();        
     return nSize;
 }
 int getSouthSize()   {
     int sSize = southTrafficQueue.size();      
-
     return sSize;
 }
 int main(int argc, char* argv[]) {
@@ -314,7 +282,8 @@ int main(int argc, char* argv[]) {
         return -1;
     }
     int cumCarsNum = stoi(argv[1]);
-
+    srand(time(nullptr));//set rand seed
+    
     //create thread for 2 producers (North and South car generator), 1 consumer (flag person)
     pthread_t consumerThread;
     pthread_t producerThreadN;
@@ -325,7 +294,6 @@ int main(int argc, char* argv[]) {
 
     //initialize pthread mutex locks
     pthread_mutex_init(&mutexCarLog, NULL);
-    pthread_mutex_init(&mutexCarProduce, NULL);
     pthread_mutex_init(&mutexQueue, NULL);
 
     //setting up Consumer(flagperson) thread function with thread
@@ -357,11 +325,11 @@ int main(int argc, char* argv[]) {
         perror("Pthread_join failed");
         exit(-2);
     }
+
     
     //deleting pthread locks
+    pthread_mutex_destroy(&mutexCarLog);
     pthread_mutex_destroy(&mutexQueue);
-    pthread_mutex_destroy(&mutexCarLog);      // destroy locks and semaphores
-    pthread_mutex_destroy(&mutexCarProduce);
 
     //delete semaphores
     sem_destroy(&isEmpty);
